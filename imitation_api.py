246a77ffc806f90af4180f26efc5f40e0c703213
Rayen023 <rayenghali02@gmail.com>
2025-07-08 16:17:37 -0300

    Add imitation API for SO101 follower and leader robots

---
+++ imitation_api.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
+from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
+from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
+
+robot_config = SO101FollowerConfig(
+    port="/dev/ttyACM1",
+    id="my_follower_arm",
+)
+
+teleop_config = SO101LeaderConfig(
+    port="/dev/ttyACM0",
+    id="my_leader_arm",
+)
+
+robot = SO101Follower(robot_config)
+teleop_device = SO101Leader(teleop_config)
+robot.connect()
+teleop_device.connect()
+
+while True:
+    action = teleop_device.get_action()
+    robot.send_action(action)
+    robot.send_action(action)
+    robot.send_action(action)
